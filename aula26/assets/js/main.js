da0bce974cb81dd230ea84d2038110e6a0a7655c
Michel Hoffmann <mic.hoff.jr@gmail.com>
2023-03-31 05:59:03 -0500

    .

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -17,7 +17,38 @@ function meuEscopo() {
       setResultado("Altura invalida!", false);
       return;
     }
+
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+
+    const msg = `Seu IMC é ${imc} ${nivelImc}`;
+    setResultado(msg, true);
+
+    console.log(imc, nivelImc);
   });
+
+  function getNivelImc(imc) {
+    const nivel = [
+      "Abaixo do peso",
+      "Peso Normal",
+      "Acima do peso",
+      "Obesidade Grau I",
+      "Obesidade Grau II",
+      "Obesidade Grau III",
+    ];
+
+    if (imc >= 39.9) return nivel[5];
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
+  }
+
+  function getImc(peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+  }
 
   function criaP() {
     const p = document.createElement("p");
@@ -28,6 +59,13 @@ function meuEscopo() {
     const resultado = document.querySelector("#resultado");
     resultado.innerHTML = "";
     const p = criaP();
+
+    if (isValid) {
+      p.classList.add("paragrafo-resultado");
+    } else {
+      p.classList.add("paragrafo-resultado-erro");
+    }
+
     p.innerHTML = msg;
     resultado.appendChild(p);
   }
