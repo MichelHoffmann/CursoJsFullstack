d27db1750675b72978e7796227da067906288759
Michel Hoffmann <mic.hoff.jr@gmail.com>
2023-04-01 06:42:31 -0500

    .

--- aula30/assets/js/main.js
+++ aula30/assets/js/main.js
@@ -3,18 +3,66 @@ function meuEscopo() {
   const data = new Date();
   const dataBrasil = dataFormatada(data);
 
+  function diaSemanaF(dia) {
+    switch (dia) {
+      case 0:
+        return "Domingo";
+      case 1:
+        return "Segunda-Feira";
+      case 2:
+        return "Terça-Feira";
+      case 3:
+        return "Quarta-Feira";
+      case 4:
+        return "Quinta-Feira";
+      case 5:
+        return "Sexta-Feira";
+      case 6:
+        return "Domingo";
+    }
+  }
+
+  function mesF(mes) {
+    switch (mes) {
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+      case 0:
+        return "Janeiro";
+    }
+  }
+
   function dataFormatada(data) {
-    const diasemana = data.getDay();
-    const diames = data.getDate();
+    const diaSemana = diaSemanaF(data.getDay());
+    const diaMes = data.getDate();
     const mes = data.getMonth();
     const ano = data.getFullYear();
     const hora = data.getHours();
     const min = data.getMinutes();
     const seg = data.getSeconds();
 
-    return `${dia}, `;
+    return `${diaSemana}, ${diaMes} de ${mes} de ${ano} ${hora}:${min}`;
   }
 
-  h1.innerHTML = data.getDate();
+  h1.innerHTML = dataBrasil;
 }
 meuEscopo();
