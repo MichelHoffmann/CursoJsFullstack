c059850a90c90f8d421affc429219329e610d3dd
Michel Hoffmann <mic.hoff.jr@gmail.com>
2023-05-07 19:33:23 -0500

    .

--- aula77/assets/js/main.js
+++ aula77/assets/js/main.js
@@ -1,8 +1,29 @@
-let cpfLimpo = cpf.replace(/\D+/g);
-
 function ValidaCpf(cpfEnviado) {
-  Object.defineProperty();
+ Object.defineProperty(this, "cpfLimpo", {
+   get: function () {
+     return cpfEnviado.replace(/\D+/g, "");
+ },
+});
+}
+
+ValidaCpf.prototype.valida = function() {
+  if (typeof this.cpfLimpo === 'underfined') return false
+  if (this.cpfLimpo.length !== 11) return false
+
+  const cpfParcial = this.cpfLimpo.slice(0, -2)
+  const digito1 = this.criaDigito(cpfParcial)
+  return true
+}
+
+ValidaCpf.prototype.criaDigito = function(cpfParcial) {
+  const cpfArray = Array.from(cpfParcial)
+
+  let regressivo = cpfArray.length + 1
+  const digito = cpfArray.reduce((ac, val) => {
+    regressivo--
+    return ac
+  }, 0)
 }
 
 const cpf = new ValidaCpf("823.698.282-34");
-cpf.valida();
+console.log(cpf)
