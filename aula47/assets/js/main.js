3eb57dde0fcb14db653d89b2621c4530662c9e7e
MichelHoffmann <mic.hoff.jr@gmail.com>
2023-11-01 10:33:33 -0500

    retomando os estudos

--- aula47/assets/js/main.js
+++ aula47/assets/js/main.js
@@ -1,36 +1,40 @@
-function criaHoraDosSegudos(segundos) {
-  const data = new Date(segundos * 1000);
-  return data.toLocaleTimeString("pt-BR", {
-    hour12: false,
-    timeZone: "UTC",
-  });
-}
+// function criaHoraDosSegudos(segundos) {
+//   const data = new Date(segundos * 1000);
+//   return data.toLocaleTimeString("pt-BR", {
+//     hour12: false,
+//     timeZone: "UTC",
+//   });
+// }
 
-const relogio = document.querySelector(".relogio");
-const iniciar = document.querySelector(".iniciar");
-const pausar = document.querySelector(".pausar");
-const zerar = document.querySelector(".zerar");
-let segundos = 0;
-let timer;
+// const relogio = document.querySelector(".relogio");
+// const iniciar = document.querySelector(".iniciar");
+// const pausar = document.querySelector(".pausar");
+// const zerar = document.querySelector(".zerar");
+// let segundos = 0;
+// let timer;
 
-function iniciarTimer() {
-  timer = setInterval(function () {
-    segundos++;
-    relogio.innerHTML = criaHoraDosSegudos(segundos);
-  }, 1000);
-  relogio.classList.remove("pausado");
-}
+// function iniciarTimer() {
+//   timer = setInterval(function () {
+//     segundos++;
+//     relogio.innerHTML = criaHoraDosSegudos(segundos);
+//   }, 1000);
+//   relogio.classList.remove("pausado");
+// }
 
-iniciar.addEventListener("click", iniciarTimer);
+// iniciar.addEventListener("click", iniciarTimer);
 
-pausar.addEventListener("click", function () {
-  clearInterval(timer);
-  relogio.classList.add("pausado");
-});
+// pausar.addEventListener("click", function () {
+//   clearInterval(timer);
+//   relogio.classList.add("pausado");
+// });
 
-zerar.addEventListener("click", function () {
-  clearInterval(timer);
-  segundos = 0;
-  relogio.classList.remove("pausado");
-  relogio.innerHTML = "00:00:00";
-});
+// zerar.addEventListener("click", function () {
+//   clearInterval(timer);
+//   segundos = 0;
+//   relogio.classList.remove("pausado");
+//   relogio.innerHTML = "00:00:00";
+// });
+
+
+const data = new Date(2023, 11, 1, 10, 15, 30)
+//                   ano   mes dia hora min  seconds
