f0081c5955899dd8dc9fa068b524b6a3197ccd5c
MichelHoffmann <mic.hoff.jr@gmail.com>
2023-11-02 09:17:30 -0500

    finalizando estudo da função Date()

--- aula47/assets/js/main.js
+++ aula47/assets/js/main.js
@@ -36,5 +36,13 @@
 // });
 
 
-const data = new Date(2023, 11, 1, 10, 15, 30)
-//                   ano   mes dia hora min  seconds
+const data = new Date()
+
+console.log(`Dia: ${data.getDate()}`)
+console.log(`Mês: ${data.getMonth()}`)
+console.log(`Ano: ${data.getFullYear()}`)
+console.log(`Hora: ${data.getHours()}`)
+console.log(`Min: ${data.getMinutes()}`)
+console.log(`Seg: ${data.getSeconds()}`)
+console.log(`Ms: ${data.getMilliseconds()}`)
+console.log(`Dia Semana: ${data.getDay()}`)
